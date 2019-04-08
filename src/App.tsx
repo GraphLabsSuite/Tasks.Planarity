56fcbf4ece4d72adb1e5c8a83ace85492d1b72ae
AnastasiiaRazheva <43729980+AnastasiiaRazheva@users.noreply.github.com>
2019-04-09 02:14:46 +0300

    Add files via upload
    Test comit. 
    Aadded a short description of the future module in ToolButtonList.prototype.help

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import * as React from 'react';
-import {TaskTemplate} from 'graphlabs.core.template';
+import { store, TaskTemplate, TaskToolbar, ToolButtonList } from 'graphlabs.core.template';
 
 class App extends TaskTemplate {
+
+    getTaskToolbar() {
+        TaskToolbar.prototype.getButtonList = () => {
+            ToolButtonList.prototype.help = () => `В данном задании будет выполняться проверка 
+            планарности графа.`;
+            return ToolButtonList;
+        };
+        return TaskToolbar;
+    }
 }
 
 export default App;
